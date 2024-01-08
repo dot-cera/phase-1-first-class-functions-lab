3bc2e5f621a0495972d0c23e62bf461062eefa86
dot-cera <ritaclaris980@gmail.com>
2024-01-08 13:39:55 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+ };
+
+ const returnLastTwoDrivers = function(array){
+     return array.slice(-2);
+ };
+
+ let selectingDrivers = [];
+ selectingDrivers.push(returnFirstTwoDrivers);
+ selectingDrivers.push(returnLastTwoDrivers);
+ 
+ function createFareMultiplier(value){
+     return function(){
+         return (value * 5);
+     };
+ }
+
+ function fareDoubler(fare){
+     return (2*fare);
+ }
+
+ function fareTripler(fare){
+     return (3*fare);
+ }
+ 
+ function selectDifferentDrivers(array,driverFunction){
+     return driverFunction(array);
+ }
